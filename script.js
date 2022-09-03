dc14658728cb6b9dcb451bc4008afa94e902630f
Taraneh Qandi <52621552+tqandi@users.noreply.github.com>
2022-09-04 02:20:57 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.navbar-toggler').on('click',function() {
+    $(this).toggleClass('menu-open');
+})
+// $(window).bind('beforeunload', function test(){
+//     // showExitAlert();
+//     return "Are you sure you want to leave?";
+//   });
+function showAlert() {
+    // alert ("Welcome!");
+  }
+  function showExitAlert() {
+    // confirm ("You Are Leaving!");
+    // return "Are you sure you want to leave?";
+    // test();
+}
+
+// window.onunload=function(){
+//     confirm ("You Are Leaving!");
+// }
+//   $(body).bind('beforeunload', function(){
+//     return 'Are you sure you want to leave?';
+//   });
+
+window.onbeforeunload = function(){
+    // return 'Are you sure you want to leave?';
+    //   confirm("confim");
+  };
+
